16157259da02b59a9c935d9458d820047af44367
Nicolas <133047323+viato115@users.noreply.github.com>
2023-05-24 23:41:03 +0200

    Changed WhatsApp to ChatGPT.

--- config.js
+++ config.js
@@ -77,9 +77,9 @@ const CONFIG = {
 		},
 		{
 			id: '5',
-			name: 'WhatsApp',
+			name: 'ChatGPT',
 			icon: 'message-circle',
-			link: 'https://web.whatsapp.com',
+			link: 'https://chat.openai.com',
 		},
 		{
 			id: '6',
